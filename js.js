9302bfbdd1885b6303c169890fef0602ff4cd790
VolDam256 <63514298+VolDam256@users.noreply.github.com>
2020-04-14 00:10:44 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+﻿JavaScript
+<script type="text/javascript">
+var slideshow=new TINY.slider.slide('slideshow',{
+	id:'slider',
+	auto:4,
+	resume:false,
+	vertical:false,
+	navid:'pagination',
+	activeclass:'current',
+	position:0,
+	rewind:false,
+	elastic:true,
+	left:'slideleft',
+	right:'slideright'
+});
+</script>
